7ab0ab951174df57d42ee5eda79f953bcfc64a4b
Alexander Katin <aleksandr.katin@gmail.com>
2018-08-12 14:28:57 +0300

    💅🏼 basic replication of a switch

---
+++ src/components/Switcher/Switcher.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import styles from './Switcher.module.scss';
+
+const Switcher = () => (
+  <fieldset className={styles.switcher}>
+    <div className={styles.inner}>
+      <label className={styles.text__label}>
+        Show me what you got
+      </label>
+
+      <div className={styles.switcher__holder}>
+        
+        <label>
+          <input className={styles.shadow__switcher} type={'checkbox'} name={'allow_something'} value={'1'} />
+          <span className={styles.switcher__body}></span>
+        </label>
+
+      </div>
+
+    </div>
+  </fieldset>
+);
+
+export default Switcher;
