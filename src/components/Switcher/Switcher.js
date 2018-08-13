eab6987b0fe3f28798f853a9da5f7bcb2439afa3
Alexander Katin <aleksandr.katin@gmail.com>
2018-08-13 11:34:51 +0300

    Start working with states

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -3,24 +3,42 @@ import Link from 'gatsby-link';
 
 import styles from './Switcher.module.scss';
 
-const Switcher = (props) => (
-  <fieldset className={styles.switcher}>
-    <div className={styles.inner}>
-      <label className={styles.text__label}>
-        {props.labelText}
-      </label>
+class Switcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {labelText: 'Show me what you\'ve got!', isOn: false}
+  }
 
-      <div className={styles.switcher__holder}>
-        
-        <label>
-          <input className={styles.shadow__switcher} type={'checkbox'} name={'allow_something'} value={'1'} />
-          <span className={styles.switcher__body}></span>
-        </label>
+  render() {
+    return (
 
-      </div>
+      <fieldset className={styles.switcher} isOn={this.state.isOn} onClick={this.handleClick.bind(this)}>
+        <div className={styles.inner}>
+          <label className={styles.text__label}>
+            {this.state.labelText}
+          </label>
 
-    </div>
-  </fieldset>
-);
+          <div className={styles.switcher__holder}>
+            
+            <label>
+              <input className={styles.shadow__switcher} type={'checkbox'} name={'allow_something'} value={'1'} />
+              <span className={styles.switcher__body}></span>
+            </label>
+
+          </div>
+
+        </div>
+      </fieldset>    
+
+    )
+  }
+
+  handleClick() {
+    this.setState = {
+      isOn: true
+    }
+  }
+
+}
 
 export default Switcher;
