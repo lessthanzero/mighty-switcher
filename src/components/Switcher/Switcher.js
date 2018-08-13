3236abd9de2c36c9722e5176ca0e4445320f4c6b
Alexander Katin <aleksandr.katin@gmail.com>
2018-08-13 17:33:37 +0300

    😊 Switcher now switches

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import classNames from 'classnames';
 import styles from './Switcher.module.scss';
+
 
 class Switcher extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {labelText: 'Show me what you\'ve got!', isOn: false}
+    this.state = {
+      labelText: 'Show me what you got!', 
+      active: false
+    };
   }
 
   render() {
+
+    let switcherBodyClasses = classNames({
+      [styles.switcher__body]: true,
+      [styles.is__active]: this.state.active
+    });
+
     return (
 
-      <fieldset className={styles.switcher} isOn={this.state.isOn} onClick={this.handleClick.bind(this)}>
+
+      <fieldset className={styles.switcher}>
         <div className={styles.inner}>
           <label className={styles.text__label}>
             {this.state.labelText}
@@ -22,7 +34,7 @@ class Switcher extends React.Component {
             
             <label>
               <input className={styles.shadow__switcher} type={'checkbox'} name={'allow_something'} value={'1'} />
-              <span className={styles.switcher__body}></span>
+              <span className={switcherBodyClasses} onClick={this.toggleClass.bind(this)}></span>
             </label>
 
           </div>
@@ -33,11 +45,10 @@ class Switcher extends React.Component {
     )
   }
 
-  handleClick() {
-    this.setState = {
-      isOn: true
-    }
-  }
+  toggleClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  };
 
 }
 
